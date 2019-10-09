0e354aa8efa166263417030b60d23ff63a8b9b23
jeyn99 <jane.repollo@student.passerellesnumeriques.org>
2019-10-09 12:14:05 +0800

    To be checked

--- DB_Crud/src/db_crud/DB_Crud.java
+++ DB_Crud/src/db_crud/DB_Crud.java
@@ -6,6 +6,7 @@
 package db_crud;
 
 import java.sql.SQLException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -19,123 +20,112 @@ public class DB_Crud {
      */
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         DBCheck db = new DBCheck();
+        boolean flag = false;
         Account account = new Account();
         PersonalInformation info = new PersonalInformation();
         Scanner input = new Scanner(System.in);
         db.connection();
-        //CreateAccount
-
-        //db.accountRetrieve();
-//        db.accountSearch(7);
-        //db.personRetrieve();
-//        db.personUpdate(4);
-        //db.personRetrieve();
-        //db.courseDelete(1);
-        //db.courseRetrieve();
-        boolean flag = true;
+        flag = true;
         while (flag) {
-            displayMenu();
-            System.out.print("Choice: ");
-            int choice = input.nextInt();
-            switch (choice) {
-                case 1: // create
-                    while (true) {
-                        retrieve();
-                        System.out.print("Choice: ");
-                        choice = input.nextInt();
-                        switch (choice) {
-                            case 1:
-                                account.checkUsername();
-                                account.passValidation();
-                                db.account(account.getUsername(), account.getPassword());
-                                break;
-                            case 2:
-                                System.out.print("Enter ACCOUNTID: ");
-                                int id = input.nextInt();
-                                db.personCreate(id);
-                                break;
-                            case 3:
-                                System.out.print("Enter ACCOUNTID: ");
-                                id = input.nextInt();
-                                db.courseCreate(id);
-                                break;
-                            default:
-                                System.out.println("Invalid!!\n");
-                                break;
-                        }
+            try {
+                while (flag) {
+                    displayMenu();
+                    System.out.print("Choice: ");
+                    int choice = input.nextInt();
+                    switch (choice) {
+                        case 1: // create
+                            retrieveAll();
+                            System.out.print("Create: ");
+                            choice = input.nextInt();
+                            switch (choice) {
+                                case 1://create account
+                                    System.out.println("CREATE ACCOUNT\n");
+                                    account.checkUsername();
+                                    account.passValidation();
+                                    db.account(account.getUsername(), account.getPassword());
+                                case 2://personalInfo
+                                    System.out.println("CREATE PERSONAL INFORMATION\n");
+                                    System.out.print("ACCOUNT ID: ");
+                                    choice = input.nextInt();
+                                    db.personCreate(choice);
+                                    break;
+                                case 3: //schedule
+                                    System.out.println("CREATE SCHEDULE\n");
+                                    System.out.print("ACCOUNT ID: ");
+                                    choice = input.nextInt();
+                                    db.courseCreate(choice);
+                                    break;
+                                default:
+                                    System.out.println("Invalid!!");
+                                    break;
+                            }
+                            break;
+                        case 2: //Retrieve
+                            db.accountRetrieve();
+                            db.personRetrieve();
+                            db.courseRetrieve();
+                            break;
+                        case 3: //Update
+                            retrieveAll();
+                            System.out.print("\n\nUPDATE??\nChoice: ");
+                            choice = input.nextInt();
+                            switch (choice) {
+                                case 1:
+                                    System.out.println("UPDATE ACCOUNT\n");
+                                    System.out.print("ACCOUNT ID: ");
+                                    choice = input.nextInt();
+                                    db.accountUpdate(choice);
+                                    break;
+                                case 2:
+                                    System.out.println("UPDATE PERSONAL INFO\n");
+                                    System.out.print("ACCOUNT ID: ");
+                                    choice = input.nextInt();
+                                    db.personUpdate(choice);
+                                    break;
+                                case 3:
+                                    System.out.println("UPDATE SCHEDULE\n");
+                                    System.out.print("ACCOUNT ID: ");
+                                    choice = input.nextInt();
+                                    db.courseUpdate(choice);
+                                    break;
+                                default:
+                                    System.out.println("Invalid!!");
+                                    break;
+                            }
+                            break;
+                        case 4: //Delete
+                            System.out.print("\nDELETE\n\tACCOUNT ID: ");
+                            choice = input.nextInt();
+                            db.personDelete(choice);
+                            db.courseDelete(choice);
+                            break;
+                        case 5:
+                            flag = false;
+                            break;
+                        default:
+                            System.out.println("Invalid!\n");
                     }
-                    
-                case 2: //Retrieve
-                    while (true) {
-                        retrieve();
-                        System.out.print("Choice: ");
-                        choice = input.nextInt();
-                        switch (choice) {
-                            case 1:
+                }
 
-                                break;
-                            case 2:
-
-                                break;
-                            case 3:
-                                break;
-                        }
-                        break;
-                    }
-                case 3: //Update
-                    while (true) {
-                        retrieve();
-                        System.out.print("Choice: ");
-                        choice = input.nextInt();
-                        switch (choice) {
-                            case 1:
-                                System.out.print("Enter Account ID: ");
-                                choice = input.nextInt();
-
-                                break;
-                            case 2:
-
-                                break;
-                            case 3:
-                                break;
-                        }
-                        break;
-                    }
-
-                case 4: //Delete
-                    while (true) {
-                        retrieve();
-                        System.out.print("Choice: ");
-                        choice = input.nextInt();
-                        switch (choice) {
-                            case 1:
-                                System.out.print("Enter Account ID: ");
-                                choice = input.nextInt();
-
-                                break;
-                            case 2:
-
-                                break;
-                            case 3:
-                                break;
-                        }
-                        break;
-                    }
-
-                case 5:
-                    flag = false;
-
+            } catch (InputMismatchException e) {
+                System.out.println("Input Mismatch");
+                break;
+            } finally {
+                db.close();
             }
         }
-        db.close();
 
     }
 
     public static void displayMenu() {
-        System.out.println("\nWelcome\n\n[1] Crud\n[2] Retrieve\n[3] Update\n[4] Delete\n[5] Exit\n\n");
+        System.out.println("\nWelcome\n\n[1] Create\n[2] Retrieve\n[3] Update\n[4] Delete\n[5] Exit\n\n");
     }
 
     public static void retrieve() {
+        System.out.println("\n[1] Personal Information\n[2] Subject\n\n");
+    }
+
+    public static void retrieveAll() {
         System.out.println("\n[1] Account\n[2] Personal Information\n[3] Subject\n\n");
     }
 
